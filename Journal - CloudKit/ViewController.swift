9ddd55253154a92f72ebbec39050a0b7f0b18d5a
dnstrach <dnstrach@gmail.com>
2023-01-31 11:42:06 -0800

    Initial Commit

---
+++ Journal - CloudKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journal - CloudKit
+//
+//  Created by Dominique Strachan on 1/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
